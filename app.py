7b67db75fbe98c8404457383135da32c50ac356a
Raj Jikadra <64123004+Raj752001@users.noreply.github.com>
2022-07-24 14:41:05 +0000

    Added Experiences page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
@@ -6,16 +6,20 @@ app = Flask(__name__)
 url='https://drive.google.com/file/d/1E-GOmtDqSlXEy4ARthIJwzvnmpY56Z0i/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 df = pd.read_csv(url)
-df.dropna(axis=0, inplace=True, how='all')
+df = df.dropna(axis=0, how='all').reset_index()
 
 @app.route("/")
-def hello():
-	return df.to_html()
+def home_page():
+    return render_template('listing.html', results=df.to_dict(orient='records'), heading = 'All Experiences')
 
-@app.route("/search/<name>")
-def search_companies(name: str):
-	# return df.loc[df["Placed Company name"].str.contains(name, False)].to_json(orient='records')
-	return render_template('listing.html', results=df.loc[df["Placed Company name"].str.contains(name, False)].to_dict(orient='records'))
+@app.route("/search")
+def search_companies():
+    name = request.args.get('name')
+    return render_template('listing.html', results=df.loc[df["Placed Company name"].str.contains(name, False)].to_dict(orient='records'), heading = 'Search Results: ' + name)
+
+@app.route("/experience/<index>")
+def view_experience(index):
+    return render_template('experience.html', result=df.iloc[int(index), 2:].to_dict())
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
